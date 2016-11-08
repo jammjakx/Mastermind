077ba601094ba34f033f7701ac902edc5fcd68b2
jammjakx <ralphverspui@live.nl>
2016-11-08 20:39:30 +0100

    Add files via upload

---
+++ Mastermind/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Mastermind
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Guessbox1 = new System.Windows.Forms.TextBox();
+            this.Checkbutton = new System.Windows.Forms.Button();
+            this.Guessbox2 = new System.Windows.Forms.TextBox();
+            this.Guessbox4 = new System.Windows.Forms.TextBox();
+            this.Guessbox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Guessbox1
+            // 
+            this.Guessbox1.Location = new System.Drawing.Point(12, 12);
+            this.Guessbox1.Name = "Guessbox1";
+            this.Guessbox1.Size = new System.Drawing.Size(23, 20);
+            this.Guessbox1.TabIndex = 0;
+            this.Guessbox1.TextChanged += new System.EventHandler(this.Guessbox1_TextChanged);
+            // 
+            // Checkbutton
+            // 
+            this.Checkbutton.Location = new System.Drawing.Point(12, 38);
+            this.Checkbutton.Name = "Checkbutton";
+            this.Checkbutton.Size = new System.Drawing.Size(75, 23);
+            this.Checkbutton.TabIndex = 1;
+            this.Checkbutton.Text = "Controleren";
+            this.Checkbutton.UseVisualStyleBackColor = true;
+            this.Checkbutton.Click += new System.EventHandler(this.Checkbutton_Click);
+            // 
+            // Guessbox2
+            // 
+            this.Guessbox2.Location = new System.Drawing.Point(41, 12);
+            this.Guessbox2.Name = "Guessbox2";
+            this.Guessbox2.Size = new System.Drawing.Size(23, 20);
+            this.Guessbox2.TabIndex = 2;
+            this.Guessbox2.TextChanged += new System.EventHandler(this.Guessbox2_TextChanged);
+            // 
+            // Guessbox4
+            // 
+            this.Guessbox4.Location = new System.Drawing.Point(99, 12);
+            this.Guessbox4.Name = "Guessbox4";
+            this.Guessbox4.Size = new System.Drawing.Size(23, 20);
+            this.Guessbox4.TabIndex = 4;
+            this.Guessbox4.TextChanged += new System.EventHandler(this.Guessbox4_TextChanged);
+            // 
+            // Guessbox3
+            // 
+            this.Guessbox3.Location = new System.Drawing.Point(70, 12);
+            this.Guessbox3.Name = "Guessbox3";
+            this.Guessbox3.Size = new System.Drawing.Size(23, 20);
+            this.Guessbox3.TabIndex = 5;
+            this.Guessbox3.TextChanged += new System.EventHandler(this.Guessbox3_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.Guessbox3);
+            this.Controls.Add(this.Guessbox4);
+            this.Controls.Add(this.Guessbox2);
+            this.Controls.Add(this.Checkbutton);
+            this.Controls.Add(this.Guessbox1);
+            this.Name = "Form1";
+            this.Text = "Mastermind";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Guessbox1;
+        private System.Windows.Forms.Button Checkbutton;
+        private System.Windows.Forms.TextBox Guessbox2;
+        private System.Windows.Forms.TextBox Guessbox4;
+        private System.Windows.Forms.TextBox Guessbox3;
+    }
+}
+
---
+++ Mastermind/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mastermind
+{
+    public partial class Form1 : Form
+    {
+        private string solution1;
+        private string solution2;
+        private string solution3;
+        private string solution4;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Random random = new Random();
+            solution1 = random.Next(1, 6).ToString();
+            solution2 = random.Next(1, 6).ToString();
+            solution3 = random.Next(1, 6).ToString();
+            solution4 = random.Next(1, 6).ToString();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Checkbutton_Click(object sender, EventArgs e)
+        {
+            //Guessbox1
+            if (Guessbox1.Text == solution1)
+            {
+                Guessbox1.BackColor = Color.LightGreen;
+            }
+            else if (Guessbox1.Text == solution2 || Guessbox1.Text == solution3 || Guessbox1.Text == solution4) 
+            {
+                Guessbox1.BackColor = Color.Yellow;
+            }
+            else
+            {
+                Guessbox1.BackColor = Color.LightGray;
+            }
+
+            //Guessbox2
+            if (Guessbox2.Text == solution2)
+            {
+                Guessbox2.BackColor = Color.LightGreen;
+            }
+            else if (Guessbox2.Text == solution1 || Guessbox2.Text == solution3 || Guessbox2.Text == solution4)
+            {
+                Guessbox2.BackColor = Color.Yellow;
+            }
+            else
+            {
+                Guessbox2.BackColor = Color.LightGray;
+            }
+
+            //Guessbox3
+            if (Guessbox3.Text == solution3)
+            {
+                Guessbox3.BackColor = Color.LightGreen;
+            }
+            else if (Guessbox3.Text == solution2 || Guessbox3.Text == solution1 || Guessbox3.Text == solution4)
+            {
+                Guessbox3.BackColor = Color.Yellow;
+            }
+            else
+            {
+                Guessbox3.BackColor = Color.LightGray;
+            }
+
+            //Guessbox4
+            if (Guessbox4.Text == solution4)
+            {
+                Guessbox4.BackColor = Color.LightGreen;
+            }
+            else if (Guessbox4.Text == solution2 || Guessbox4.Text == solution3 || Guessbox4.Text == solution1)
+            {
+                Guessbox4.BackColor = Color.Yellow;
+            }
+            else
+            {
+                Guessbox4.BackColor = Color.LightGray;
+            }
+        }
+
+        private void Guessbox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Guessbox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Guessbox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Guessbox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
